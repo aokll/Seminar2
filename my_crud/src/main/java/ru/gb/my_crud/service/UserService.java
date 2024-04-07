2560445b0fed732ac8f3f162408a2fafb689fe0f
aokll <pastuhov_1992@mail.ru>
2024-04-07 22:04:54 +0300

    sem2

---
+++ my_crud/src/main/java/ru/gb/my_crud/service/UserService.java
@@ -0,0 +1,33 @@
+package ru.gb.my_crud.service;
+
+import org.springframework.stereotype.Service;
+import ru.gb.my_crud.model.User;
+import ru.gb.my_crud.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+
+    public void deleteById(int id){
+        userRepository.deleteById(id);
+    }
+    public void updateById(int id, String firstName, String lastName){
+        userRepository.updateById(id, firstName, lastName);
+    }
+}
+
